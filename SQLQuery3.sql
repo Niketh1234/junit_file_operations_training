a11acc30d89355369c807470c19fa601b89c6cc1
dniketh <dniketh@actalentservices.com>
2024-08-17 18:01:57 +0000

    Modification for SQLQuery3

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -2,15 +2,16 @@
 create table visitor(
 	vid int primary key identity(1,1),
 	vname varchar(15),
+	age tinyint check (age between 1 and 150),
 	purpose varchar(10),
 	gender varchar(7) check (gender in ('M','F','m','f','female','male','Female','Male')),
 	dept varchar(10),
 	logintime datetime default getdate(),
 	phoneno char(10) check (phoneno like '[6-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
 )
-insert into visitor(vname,purpose,gender,dept,phoneno) values('niketh','training','male','fsd','9234567890'),
-('saketh','eating','male','bpo','8987654321'),
-('akshaya','sleeping','f',null,'9010101010')
+insert into visitor(vname,age,purpose,gender,dept,phoneno) values('niketh','21','training','male','fsd','9234567890'),
+('saketh','32','eating','male','bpo','8987654321'),
+('akshaya','145','sleeping','f',null,'9010101010')
 
 select * from visitor
 
